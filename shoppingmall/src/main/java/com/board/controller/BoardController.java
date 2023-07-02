31a6203b755ea010da3c57855f3fedb6975a130c
MOONSOPU <kimsopu@gmail.com>
2023-07-03 00:33:56 +0900

    20230703 페이징

--- shoppingmall/src/main/java/com/board/controller/BoardController.java
+++ shoppingmall/src/main/java/com/board/controller/BoardController.java
@@ -111,6 +111,15 @@ public class BoardController {
 	 // 표시되는 페이지 번호 중 마지막 번호
 	 int endPageNum = (int)(Math.ceil((double)num / (double)pageNum_cnt) * pageNum_cnt);
 	   
+	 // 표시되는 페이지 번호 중 첫번째 번호
+	 int startPageNum = endPageNum - (pageNum_cnt - 1);
+	 
+	// 마지막 번호 재계산
+	 int endPageNum_tmp = (int)(Math.ceil((double)count / (double)pageNum_cnt));
+	  
+	 if(endPageNum > endPageNum_tmp) {
+	  endPageNum = endPageNum_tmp;
+	 }
 	 
 	 List<BoardVO> list = service.listPage(displayPost, postNum);
 	 model.addAttribute("list", list);   
